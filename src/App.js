0cb9033f5e2317743e5232884fdd5df759209bd7
chrysanthemum <asingiser@gmail.com>
2023-05-04 12:06:58 -0400

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
-function App() {
-  return <div className='App'>Hello World</div>;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function App() {
+    const [data , setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+      axios(`https://v3.football.api-sports.io/leagues`)
+        .then((response) => {
+          setData(response.data)
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error)
+          setError(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }, [])
+
+    if (loading) return "Loading..."
+    if (error) return "Error!!"
+  
+  return(
+    <>
+      {console.log(data)}
+    </>
+  )
 }
-export default App;
